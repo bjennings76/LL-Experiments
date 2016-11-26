c5f9ca0be57cb55f263713804d5dc37405f6ef39
Brian Dean Jennings <brian@bdjnews.com>
2016-11-26 15:14:01 +1300

    Added character creator test models and scripts (WIP)

---
+++ Assets/Character Creator/Scripts/BodySwapper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BodySwapper : MonoBehaviour {
+  private MeshRenderer[] m_Renderers;
+  private Material[] m_Materials;
+
+  private void Start() {
+    m_Renderers = GetComponentsInChildren<MeshRenderer>();
+    m_Materials = Resources.LoadAll<Material>("Characters/Materials");
+  }
+
+  public void NextModel() {
+    
+  }
+
+  public void NextMaterial() {
+    
+  }
+}
