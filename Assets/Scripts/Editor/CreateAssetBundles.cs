6622e0f952a3c87920769e64ab58415ef93d5b48
Brian Dean Jennings <brian@bdjnews.com>
2016-11-26 15:14:56 +1300

    Added a script to create asset bundles, though we probably won't really use them just yet.

---
+++ Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+
+public static class CreateAssetBundles {
+  [MenuItem("Assets/Build AssetBundles")]
+  private static void BuildAssetBundles() {
+    CheckDirectory("Assets/AssetBundles/Windows");
+    CheckDirectory("Assets/AssetBundles/WebGL");
+
+    BuildPipeline.BuildAssetBundles("Assets/AssetBundles/Windows", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    BuildPipeline.BuildAssetBundles("Assets/AssetBundles/WebGL", BuildAssetBundleOptions.None, BuildTarget.WebGL);
+  }
+
+  private static void CheckDirectory(string dir) {
+    if (!Directory.Exists(dir)) {
+      Directory.CreateDirectory(dir);
+    }
+  }
+}
